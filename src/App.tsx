146efae505b39ab4c60acace540c2494229557de
nadiaGincoff <62709855+nadiaGincoff@users.noreply.github.com>
2025-11-15 16:44:07 -0300

    feat: add initial App component and styles

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center bg-linear-to-br h-screen w-screen">
+      hello patient
+    </div>
+  );
+}
+
+export default App;
